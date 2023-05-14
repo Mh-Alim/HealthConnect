8cf2f3f65c4e8aced599446f9c177df8435537e3
Alim Khan <90334104+Mh-Alim@users.noreply.github.com>
2023-05-14 18:33:30 +0530

    ui updated

--- src/components/ReviewSlider/HomeReview.js
+++ src/components/ReviewSlider/HomeReview.js
@@ -1,38 +1,39 @@
-import React from 'react'
-import "./Review.css"
-import { NavLink } from 'react-router-dom'
-import image from "../../images/user.jpg"
+import React from "react";
+import "./Review.css";
+import { NavLink } from "react-router-dom";
+import image from "../../images/default_user.png";
 
 const HomeReview = () => {
   return (
-        <div id="review">
-      
-        
-      <div className='testimonials'>
+    <div id="review">
+      <div className="testimonials">
         <div className="inner">
-          <h1 className='mf'>Reviews</h1>
+          <h1 className="mf">Reviews</h1>
           <div className="border"></div>
           <div className="review_row">
             <div className="review_col">
               <div className="testimonial pf">
                 <img src={image} alt="reviewImage" />
-                <div className="name">Full Name</div>
+                <div className="name">Alim Khan</div>
                 <div className="stars">
-                  <i className="fas fa-star"></i>
+                  <i className="fas fa-stsar"></i>
                   <i className="fas fa-star"></i>
                   <i className="far fa-star"></i>
                   <i className="far fa-star"></i>
                 </div>
 
-
-                <p className='review_para'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum obcaecati dignissimos est, architecto eveniet minima explicabo perferendis omnis quam magni non accusantium atque laboriosam? Dicta sapiente saepe voluptas tempora ipsum ipsa harum praesentium deleniti sit perferendis laborum, reiciendis at optio.</p>
-
+                <p className="review_para">
+                  This website is amazing! It's so easy to book appointments and
+                  the payment process is quick and hassle-free. I love how I can
+                  see all my appointments in one place and leave reviews to help
+                  others. Highly recommend!
+                </p>
               </div>
             </div>
             <div className="review_col">
               <div className="testimonial pf">
                 <img src={image} alt="reviewImage " />
-                <div className="name">Full Name</div>
+                <div className="name">Mukul</div>
                 <div className="stars">
                   <i className="far fa-star"></i>
                   <i className="far fa-star"></i>
@@ -40,15 +41,20 @@ const HomeReview = () => {
                   <i className="far fa-star"></i>
                 </div>
 
-
-                <p className='review_para'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum obcaecati dignissimos est, architecto eveniet minima explicabo perferendis omnis quam magni non accusantium atque laboriosam? Dicta sapiente saepe voluptas tempora ipsum ipsa harum praesentium deleniti sit perferendis laborum, reiciendis at optio.</p>
-
+                <p className="review_para">
+                  "I recently used this website to book an appointment at a
+                  clinic and I was blown away by how user-friendly it was. The
+                  appointment form was straightforward and I received a
+                  confirmation email right away. Plus, the option to leave a
+                  review is a great way to give feedback and help others make
+                  informed decisions. Kudos to the team behind this!
+                </p>
               </div>
             </div>
             <div className="review_col">
               <div className="testimonial pf">
                 <img src={image} alt="reviewImage" />
-                <div className="name">Full Name</div>
+                <div className="name">Lakshman</div>
                 <div className="stars">
                   <i className="far fa-star"></i>
                   <i className="far fa-star"></i>
@@ -56,20 +62,29 @@ const HomeReview = () => {
                   <i className="far fa-star"></i>
                 </div>
 
-
-                <p className='review_para'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum obcaecati dignissimos est, architecto eveniet minima explicabo perferendis omnis quam magni non accusantium atque laboriosam? Dicta sapiente saepe voluptas tempora ipsum ipsa harum praesentium deleniti sit perferendis laborum, reiciendis at optio.</p>
-
+                <p className="review_para">
+                  I can't say enough good things about this website. From start
+                  to finish, the process of booking an appointment and making a
+                  payment was seamless. I also appreciated the ability to see
+                  other people's reviews of the clinic before making my own
+                  appointment. This website has definitely made my life easier
+                  and I will be using it again in the future!
+                </p>
               </div>
             </div>
-            
-            
-            
           </div>
         </div>
-      <p className='pf'>wanna see more Reviews go to <NavLink to='/review' className="underline"> Review  </NavLink> section</p>
+        <p className="pf">
+          wanna see more Reviews go to{" "}
+          <NavLink to="/review" className="underline">
+            {" "}
+            Review{" "}
+          </NavLink>{" "}
+          section
+        </p>
       </div>
-  </div>
-  )
-}
+    </div>
+  );
+};
 
-export default HomeReview
+export default HomeReview;
--- src/components/WorkingProcess/WorkingProcess.js
+++ src/components/WorkingProcess/WorkingProcess.js
@@ -1,52 +1,57 @@
-import React from 'react'
-import './WorkingProcess.css'
+import React from "react";
+import "./WorkingProcess.css";
 // import { MDBIcon} from 'mdbreact';
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 
 const WorkingProcess = () => {
   return (
-    <div id='WorkingProcessMainwork-container'>
-        <div className="h6"><h4>Working Process</h4></div>
-        <div className="workHeadingText"><h2>How we works?</h2></div>
-        {/* cards starts from here */}
-        <div className="cardBody">
-
-       
+    <div id="WorkingProcessMainwork-container">
+      <div className="h6">
+        <h4>Working Process</h4>
+      </div>
+      <div className="workHeadingText">
+        <h2>How we works?</h2>
+      </div>
+      {/* cards starts from here */}
+      <div className="cardBody">
         <div className="work-container">
-                    <div className="card">
-                        <div className="content">
-                            <h2>01</h2>
-                            <h3>STEP-1</h3>
-                            <p>Click on the below button to take appointment or you can also click on the Take appointment button on the navbar </p>
-                            <NavLink to =  "#">Read More</NavLink>
-                        </div>
-                    </div>
-                </div>
-                <div className="work-container">
-                    <div className="card">
-                        <div className="content">
-                            <h2>02</h2>
-                            <h3> STEP-2 </h3>
-                            <p>Click on the below button for online fee paymnet </p>
-                            <NavLink to =  "#">Read More</NavLink>
-                        </div>
-                    </div>
-                </div>
-                <div className="work-container">
-                    <div className="card">
-                        <div className="content">
-                            <h2>03</h2>
-                            <h3>Card Three</h3>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque, incidunt omnis. Similique odio facilis perferendis rem exercitationem maxime officiis inventore totam beatae corporis quia ut dicta accusamus temporibus</p>
-                            <NavLink to =  "#">Read More</NavLink>
-                        </div>
-                    </div>
-                </div> 
+          <div className="card">
+            <div className="content">
+              <h2>01</h2>
+              <h3>STEP-1</h3>
+              <p>
+                Click on the below button to take appointment or you can also
+                click on the Take appointment button on the navbar{" "}
+              </p>
+              <NavLink to="/appointment">Appointment</NavLink>
             </div>
+          </div>
+        </div>
+        <div className="work-container">
+          <div className="card">
+            <div className="content">
+              <h2>02</h2>
+              <h3> STEP-2 </h3>
+              <p>Click on the below button for online fee paymnet </p>
+              <NavLink to="/appointment">Payment Gateway</NavLink>
+            </div>
+          </div>
+        </div>
+        <div className="work-container">
+          <div className="card">
+            <div className="content">
+              <h2>03</h2>
+              <h3>STEP-3</h3>
+              <p>Check Your Appointment Number here</p>
+              <NavLink to="/lists">Details</NavLink>
+            </div>
+          </div>
+        </div>
+      </div>
 
-            <hr />
+      <hr />
     </div>
-  )
-}
+  );
+};
 
-export default WorkingProcess
+export default WorkingProcess;
